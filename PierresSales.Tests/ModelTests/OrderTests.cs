3e294e354b4d0cf847260c8b2594ea01cd165c61
Robert Onstott <roonstott@gmail.com>
2022-12-16 12:20:29 -0800

    TEST Method: should assign incremental unique id's to orders. Pass based on prior code.  Order counter is a private static integer, so can't be cleared by a Dispose() method.

--- PierresSales.Tests/ModelTests/OrderTests.cs
+++ PierresSales.Tests/ModelTests/OrderTests.cs
@@ -14,5 +14,15 @@ namespace PierresSales.Tests
       Order newOrder1 = new Order("March 1st", "leave on porch");
       Assert.AreEqual(typeof(Order), newOrder1.GetType());       
     }
+    [TestMethod]
+    public void Constructor_AssignsUniqueId_Int()
+    {
+      Order newOrder1 = new Order("March 1st", "leave on porch");
+      Order newOrder2 = new Order("Janurary 2nd", "extra chocolate");
+      int result1 = newOrder1.OrderNumber;
+      int result2 = newOrder2.OrderNumber;
+      Assert.AreEqual(2, result1); 
+      Assert.AreEqual(3, result2);
+    }
   }
 }
