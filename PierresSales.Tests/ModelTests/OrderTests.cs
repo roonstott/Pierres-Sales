e9121c6eef4c70a1092f6aaed374e22c9a856817
Robert Onstott <roonstott@gmail.com>
2022-12-16 12:09:05 -0800

    Pass first test for Order constructor

--- PierresSales.Tests/ModelTests/OrderTests.cs
+++ PierresSales.Tests/ModelTests/OrderTests.cs
@@ -8,6 +8,11 @@ namespace PierresSales.Tests
   [TestClass]
   public class OrderTests
   {
-    
+    [TestMethod]
+    public void Constructor_CreatesInstanceOfClass_Order()
+    {
+      Order newOrder1 = new Order("March 1st", "leave on porch");
+      Assert.AreEqual(typeof(Order), newOrder1.GetType());       
+    }
   }
 }
--- PierresSales/Models/Order.cs
+++ PierresSales/Models/Order.cs
@@ -1,9 +1,30 @@
 using System.Collections.Generic;
+using System;
 
 namespace PierresSales.Models
 {
   public class Order
   {
+    public int OrderNumber { get; }
+    public string DeliverDate { get; }
+    public string SpecialInstructions { get; }    
+    public bool OrderFulfilled { get; set; }
+    public bool OrderPaid { get; set; }
+    public Dictionary<string, int> OrderItems { get; }
+
+    private static Dictionary<string, double> _priceList = new Dictionary<string, double> () {{"baguette", 3.25}, {"pastry", 2.00}, {"croissant", 2.50}, {"cookie", 1.50}};
+    private static int _orderCounter = 1;
+
+    public Order (string deliverDate, string specialInstructions)
+    {
+      string DeliverDate = deliverDate;
+      string SpecialInstructions = specialInstructions;
+      bool OrderFulfilled = false; 
+      bool OrderPaid = false;
+      OrderItems = new Dictionary<string, int> () { };
+      OrderNumber = _orderCounter; 
+      _orderCounter +=1; 
+    }
     
   }
 }
